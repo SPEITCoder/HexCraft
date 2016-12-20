f6b96cb299d939c1778b8326760a44ecb62ac188
biyue111 <biyue111@126.com>
2016-12-20 22:28:19 +0800

    add landform generator2

---
+++ Assets/TBS Framework/Scripts/HexCraft/RectHexGridGeneratorHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(RectHexGridGenerator), true)]
+public class RectHexGridGeneratorHelper : Editor {
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		RectHexGridGenerator gridGenerator = (RectHexGridGenerator)target;
+
+		if (GUILayout.Button("Generate Grid"))
+		{
+			gridGenerator.GenerateGrid();
+		}
+		if (GUILayout.Button ("Generate Landform"))
+		{
+			gridGenerator.GenerateLandform();
+		}
+		if (GUILayout.Button("Clear Grid"))
+		{
+			var children = new List<GameObject>();
+			foreach(Transform cell in gridGenerator.CellsParent)
+			{
+				children.Add(cell.gameObject);
+			}
+
+			children.ForEach(c => DestroyImmediate(c));
+		}
+	}
+}

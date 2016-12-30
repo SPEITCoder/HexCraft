f7dd0a56d91c16c1ae4f16484ab902352b9601c0
biyue111 <biyue111@126.com>
2016-12-30 13:43:55 +0800

    add assets

---
+++ Assets/LowPolyMedievalBuildings/Demo/Controller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	public GameObject spring;
+	public GameObject winter;
+	private bool seasonState;
+	private bool springState;
+	private bool winterState;
+	// Use this for initialization
+	void Start () {
+		seasonState = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("e") && seasonState == false) 
+		{
+			seasonState = true;
+		}
+
+		if (Input.GetKeyUp ("q") && seasonState == true) 
+		{
+			seasonState = false;
+		}
+
+		if (seasonState == false) {
+			spring.SetActive(true);
+			winter.SetActive(false);
+		} 
+		if (seasonState == true)
+		{
+			spring.SetActive(false);
+			winter.SetActive(true);
+		}
+	}
+}

460e9908e184cab35e2eae0a9887107ca5aa1a30
muyangyi <yimuyang@outlook.com>
2017-01-07 17:24:05 +0800

    bug fixed

--- Assets/TBS Framework/Scripts/HexCraft/Archer.cs
+++ Assets/TBS Framework/Scripts/HexCraft/Archer.cs
@@ -1,5 +1,34 @@
-﻿public class Archer : UnitAdv
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Archer : UnitAdv
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+        Transform archer = null;
+        foreach (Transform child in transform)
+        {
+            //Debug.Log(child.gameObject.name);
+            if (child.gameObject.name.Equals("WK_archer"))
+                archer = transform.Find("WK_archer");
+        }
+        if (archer == null)
+        {
+            Debug.Log("No Archer");
+            return;
+        }
+        var unitTexture = Resources.Load(KindomInfoHelper.getKindomInfobyNumber(PlayerNumber).SoilderTexturePath);
+        Debug.Log(KindomInfoHelper.getKindomInfobyNumber(PlayerNumber).SoilderTexturePath);
+        if (unitTexture == null)
+        {
+            Debug.Log("No Texture");
+            return;
+        }
+        archer.gameObject.GetComponentInChildren<Renderer>().material.mainTexture = unitTexture as Texture;
+    }
     protected override void Defend(Unit other, int damage)
     {
         var realDamage = damage;
--- Assets/TBS Framework/Scripts/HexCraft/Paladin.cs
+++ Assets/TBS Framework/Scripts/HexCraft/Paladin.cs
@@ -1,5 +1,34 @@
-﻿public class Paladin : UnitAdv
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Paladin : UnitAdv
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+        Transform cavalry = null;
+        foreach (Transform child in transform)
+        {
+            //Debug.Log(child.gameObject.name);
+            if (child.gameObject.name.Equals("WK_cavalry"))
+                cavalry = transform.Find("WK_cavalry");
+        }
+        if (cavalry == null)
+        {
+            Debug.Log("No Cavalry");
+            return;
+        }
+        var unitTexture = Resources.Load(KindomInfoHelper.getKindomInfobyNumber(PlayerNumber).SoilderTexturePath);
+        Debug.Log(KindomInfoHelper.getKindomInfobyNumber(PlayerNumber).SoilderTexturePath);
+        if (unitTexture == null)
+        {
+            Debug.Log("No Texture");
+            return;
+        }
+        cavalry.gameObject.GetComponentInChildren<Renderer>().material.mainTexture = unitTexture as Texture;
+    }
     protected override void Defend(Unit other, int damage)
     {
         var realDamage = damage;
